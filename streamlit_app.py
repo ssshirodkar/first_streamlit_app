b0a2ccd0b85ba20072aae37343b705657d457b6a
SagarShirodkar <ssshirodkar@gmail.com>
2022-07-03 22:54:58 +0530

    Update streamlit_app.py
    trying to add datainto snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,6 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)
+
+#going with flow
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
